990255d032ab7886ecdf2fbcc8b8fffd0349d311
Ron Yip <ronyuiyip@gmail.com>
2023-12-16 22:15:15 +0800

    test github host

--- app/Experiences/component/EventCard.tsx
+++ app/Experiences/component/EventCard.tsx
@@ -9,20 +9,27 @@ interface Props {
 
 const EventCard = ({ title, date, description }: Props) => {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td className={styles.dateCell}>{date}</td>
-          <td className={styles.dotCell}>
-            <div className={styles.dot} />
-            <div className={styles.verticalLine} />
-          </td>
-          <td className={styles.titleCell}>{title}</td>
-          <td className={styles.descriptionCell}>{description}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="justify-center">
+      <h1 className="text-lg hover:text-xl">{title}</h1>
+      <div className="h-5 w-5 rounded-full bg-white"></div>
+      <h1 className="text-base">{date}</h1>
+    </div>
+    
   );
 };
 
 export default EventCard;
+
+{/* <table>
+<tbody>
+  <tr>
+    <td className={styles.dateCell}>{date}</td>
+    <td className={styles.dotCell}>
+      <div className={styles.dot} />
+      <div className={styles.verticalLine} />
+    </td>
+    <td className={styles.titleCell}>{title}</td>
+    <td className={styles.descriptionCell}>{description}</td>
+  </tr>
+</tbody>
+</table> */}
